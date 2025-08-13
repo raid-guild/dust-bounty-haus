d4eacb7396171f70701ffdc9f97edcda5325b1ab
vdrg <vdrg@protonmail.ch>
2025-08-13 12:19:31 -0400

    Comment setResourceTag calls and fix register app scripts

--- packages/contracts/script/RegisterApp.s.sol
+++ packages/contracts/script/RegisterApp.s.sol
@@ -1,29 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.30;
 
-import { metadataSystem } from "@latticexyz/world-module-metadata/src/codegen/experimental/systems/MetadataSystemLib.sol";
-import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import {metadataSystem} from "@latticexyz/world-module-metadata/src/codegen/experimental/systems/MetadataSystemLib.sol";
+import {ResourceId, WorldResourceIdLib} from "@latticexyz/world/src/WorldResourceId.sol";
 
-import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import {StoreSwitch} from "@latticexyz/store/src/StoreSwitch.sol";
 
-import { console } from "forge-std/console.sol";
+import {console} from "forge-std/console.sol";
 
-import { Script } from "./Script.sol";
+import {Script} from "./Script.sol";
 
 contract RegisterApp is Script {
-  function run(address worldAddress) external {
-    // Specify a store so that you can use tables directly in PostDeploy
-    StoreSwitch.setStoreAddress(worldAddress);
+    function run(address worldAddress) external {
+        // Specify a store so that you can use tables directly in PostDeploy
+        StoreSwitch.setStoreAddress(worldAddress);
 
-    startBroadcast();
+        startBroadcast();
 
-    ResourceId appNamespaceId = WorldResourceIdLib.encodeNamespace("my_namespace");
-    string memory appUrl = "https://example.com/dust-app.json";
-    console.log("Registering app with url: %s", appUrl);
+        ResourceId appNamespaceId = WorldResourceIdLib.encodeNamespace("my_namespace");
+        string memory appUrl = "https://example.com/dust-app.json";
+        console.log("Registering app with url: %s", appUrl);
 
-    metadataSystem.setResourceTag(appNamespaceId, "dust.appConfigUrl", bytes(appUrl));
-    metadataSystem.setResourceTag(appNamespaceId, "dust.spawnAppConfigUrl", bytes(appUrl));
+        revert("Remove this revert from script/RegisterApp.s.sol and uncomment the setResourceTag calls that you need");
+        // NOTE: uncomment this to register as a global app (sidebar apps)
+        // metadataSystem.setResourceTag(appNamespaceId, "dust.appConfigUrl", bytes(appUrl));
 
-    vm.stopBroadcast();
-  }
+        // NOTE: uncomment this to register as a spawn app (appears when user is choosing how to spawn)
+        // metadataSystem.setResourceTag(appNamespaceId, "dust.spawnAppConfigUrl", bytes(appUrl));
+
+        vm.stopBroadcast();
+    }
 }
